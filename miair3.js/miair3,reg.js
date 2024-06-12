cf958934856155f056b77100b61e6925ed00d5f9
miair3 <167542260+miair3@users.noreply.github.com>
2024-06-12 17:55:12 +0500

    Add files via upload

---
+++ miair3.js/miair3,reg.js
@@ -0,0 +1,21 @@
+
+  const setTheme = (theme) => {
+    const root = document.querySelector(":root");
+    root.style.setProperty("--background", theme.background);
+    root.style.setProperty("--color", theme.color);
+    root.style.setProperty("--primary-color", theme.primaryColor);
+    root.style.setProperty("--glass-color", theme.glassColor);
+  };
+  
+  const displayThemeButtons = () => {
+    const btnContainer = document.querySelector(".theme-btn-container");
+    themes.forEach((theme) => {
+      const div = document.createElement("div");
+      div.className = "theme-btn";
+      div.style.cssText = `background: ${theme.background}; width: 25px; height: 25px`;
+      btnContainer.appendChild(div);
+      div.addEventListener("click", () => setTheme(theme));
+    });
+  };
+  
+  displayThemeButtons();
